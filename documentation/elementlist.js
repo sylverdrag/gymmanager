db1b864111fe45bbfaee9000ab93346e77e0cec7
Sylvain Galibert <dev@preptags.com>
2023-05-04 13:22:27 +0200

    Auto generated documentation
    Signed-off-by: Sylvain Galibert <dev@preptags.com>

---
+++ documentation/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","array2table()"],["c","data_formatting_class"],["c","dBug"],["c","form"],["f","generate_contracts()"],["f","get_all_3_letters_acronyms()"],["f","get_all_4_letters_acronyms()"],["c","gym_edits_class"],["c","gym_manager_class"],["c","gym_reports_class"],["f","is_date()"],["f","login()"]];
